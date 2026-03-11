e0a810344d96998fc2ea416f91688a9dd7f26d83
w19andrian <wempiandrian@gmail.com>
2026-03-11 20:34:09 +0100

    chore: re-organize fugitive keymaps nvim

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,9 +1,8 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "<leader>gg", "<cmd>Git<CR>", { desc = "Fu[Git]ive main console" })
-		vim.keymap.set("n", "<leader>gp", "<cmd>Git pull<CR>", { desc = "Fu[Git]ive [p]ull (rebase)" })
-		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>", { desc = "Fu[Git]ive [c]ommit console" })
+		vim.keymap.set("n", "<leader>g<CR>", "<cmd>Git<CR>", { desc = "Fu[Git]ive main console" })
+		vim.keymap.set("n", "<leader>gp", "<cmd>Git pull<CR>", { desc = "Fu[Git]ive [p]ull" })
 		vim.keymap.set("n", "<leader>gP", "<cmd>Git push<CR>", { desc = "Fu[Git]ive [P]ush" })
 	end,
 }
